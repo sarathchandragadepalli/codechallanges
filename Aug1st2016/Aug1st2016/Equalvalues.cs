be6ac3e01aeff9f8a8c7d390a9dc466ee806efb3
sarathchandragadepalli <sarathchandra.gadepalli@campsystems.com>
2016-08-03 22:24:05 +0530

    Add files via upload

---
+++ Aug1st2016/Aug1st2016/Equalvalues.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1staug2016
+{
+    class Equalvalues
+    {
+        static void Main(string[] args)
+        {
+            try {
+                Console.WriteLine("Enter the values of array seperated with ,");
+                int[] values = Array.ConvertAll(Console.ReadLine().Split(','), Int32.Parse);
+                Equalvalues eq = new Equalvalues();
+                eq.isEqual(values);
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Enter proper values");
+            }
+            Console.ReadLine();
+
+        }
+
+        public bool isEqual(int[] values){
+            int sum = 0,sumLeft=0,sumRight;
+            sum = values.Sum();
+             sumRight = sum;
+             for (int i = 0; i < values.Length; i++)
+            {
+                sumLeft += values[i];
+                if (sumLeft == sumRight)
+                {
+                    Console.WriteLine("TEST CASE : PASSED & The index is " + (i+1));
+                    return true;
+                }
+                sumRight -= values[i];
+            }
+             Console.WriteLine("TEST CASE : FAILED");
+             return false;
+        }
+    }
+}
