566e5aa1da28856d48ab0181707437e7e8a272d9
sarathchandragadepalli <sarathchandra.gadepalli@campsystems.com>
2016-09-22 23:13:57 +0530

    Add files via upload

---
+++ 10sep2016/10sep2016/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Threading;
+
+namespace _10sep2016
+{
+    public class CalculateDisplcaement
+    {
+        static void Main(string[] args)
+        {
+            TakeInputandcalculateLogic();
+            Console.ReadLine();
+        }
+
+        public string CalculateLogic(int[,] pointsarray, Point p, Point q)
+        {
+            bool path = false;
+            Queue<Point> visitedQueue = new Queue<Point>();
+            Queue<Point> yettoVisitQueue = new Queue<Point>();
+            var noofRows = pointsarray.GetLength(0);
+            var noofcolumns = pointsarray.GetLength(1);
+            int count = 0;
+            try
+            {
+                if (pointsarray[q.X, q.Y] == 0)
+                {
+                    Console.WriteLine("No : No path exists between the points");
+                    return "No";
+                }
+                yettoVisitQueue.Enqueue(p);
+                visitedQueue.Enqueue(p);
+                Dictionary<Point, int> shDictionary = new Dictionary<Point, int>();
+                shDictionary.Add(p, count);
+                while (yettoVisitQueue.Count > 0)
+                {
+                    p = yettoVisitQueue.Dequeue();
+
+                    if (p.X == q.X && p.Y == q.Y)
+                    {
+                        Console.WriteLine(" Yes Path exists");
+                        path = true;
+                        Console.WriteLine("shortest distance is:" + shDictionary[p]);
+                        return "Yes " + shDictionary[p];
+                    }
+                    if (p.X - 1 >= 0 && pointsarray[p.X - 1, p.Y] == 1)
+                    {
+                        Point x = new Point(p.X - 1, p.Y);
+
+                        if (!visitedQueue.Contains(x))
+                        {
+                            visitedQueue.Enqueue(x);
+                            yettoVisitQueue.Enqueue(x);
+                            shDictionary.Add(x, shDictionary[p] + 1);
+                        }
+                    }
+                    if (p.X + 1 < noofRows && pointsarray[p.X + 1, p.Y] == 1)
+                    {
+                        Point x = new Point(p.X + 1, p.Y);
+
+                        if (!visitedQueue.Contains(x))
+                        {
+                            visitedQueue.Enqueue(x);
+
+                            yettoVisitQueue.Enqueue(x);
+                            shDictionary.Add(x, shDictionary[p] + 1);
+                        }
+                    }
+                    if (p.Y + 1 < noofcolumns && pointsarray[p.X, p.Y + 1] == 1)
+                    {
+                        Point x = new Point(p.X, p.Y + 1);
+
+                        if (!visitedQueue.Contains(x))
+                        {
+                            visitedQueue.Enqueue(x);
+                            yettoVisitQueue.Enqueue(x);
+                            shDictionary.Add(x, shDictionary[p] + 1);
+                        }
+                    }
+                    if ((p.Y - 1) >= 0 && pointsarray[p.X, p.Y - 1] == 1)
+                    {
+                        Point x = new Point(p.X, p.Y - 1);
+
+                        if (!visitedQueue.Contains(x))
+                        {
+                            visitedQueue.Enqueue(x);
+                            yettoVisitQueue.Enqueue(x);
+                            shDictionary.Add(x, shDictionary[p] + 1);
+                        }
+                    }
+                }
+
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("Please enter the input points in between the range");
+                return "please try the input within the range";
+            }
+            if (!path)
+                Console.WriteLine("No : No path exists between the points");
+            return "No";
+        }
+
+        private static void TakeInputandcalculateLogic()
+        {
+            try { 
+            Console.WriteLine("please enter the number of rows");
+            var noofRows = Convert.ToInt32(Console.ReadLine());
+                if (noofRows < 0 || noofRows >= 100)
+                {
+                    Console.WriteLine("no of rows must be greater than 0 and less than 100");
+                    noofRows = Convert.ToInt32(Console.ReadLine());
+                }
+            Console.WriteLine("please enter the number of columns");
+            var noofColumns = Convert.ToInt32(Console.ReadLine());
+                if (noofColumns < 0 || noofColumns >= 100)
+                {
+                    Console.WriteLine("no of columns must be greater than 0 and less than 100");
+                    noofColumns = Convert.ToInt32(Console.ReadLine());
+                }
+                Console.WriteLine("please enter the elements { each element followed by enter } ");
+            int[,] pointsarray = new int[noofRows, noofColumns];
+            int i;
+                /* output each array element's value */
+                for (i = 0; i < noofRows; i++)
+                {
+                    int j;
+                    for (j = 0; j < noofColumns; j++)
+                    {
+                        pointsarray[i, j] = Convert.ToInt32(Console.ReadLine());
+                        if (pointsarray[i, j] != 0 && pointsarray[i, j] != 1)
+                        {
+                            Console.WriteLine("please enter only 0's and 1's as input");
+                            return;
+                        }
+                    }
+                }
+                Console.WriteLine("please enter  two input points in array { each element followed by enter } ");
+                Point p = new Point();
+                Point q = new Point();
+                p.X = Convert.ToInt32(Console.ReadLine());
+                p.Y = Convert.ToInt32(Console.ReadLine());
+                q.X = Convert.ToInt32(Console.ReadLine());
+                q.Y = Convert.ToInt32(Console.ReadLine());
+                CalculateDisplcaement displacement = new CalculateDisplcaement();
+                displacement.CalculateLogic(pointsarray, p, q);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("please enter the proper format");
+            }
+        }
+    }
+}
---
+++ 10sep2016/10sep2016Tests/CalculateDisplcaementTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _10sep2016;
+
+namespace _10sep2016Tests
+{
+    [TestClass()]
+    public class CalculateDisplcaementTests
+    {
+        [TestMethod()]
+        public void CalculateLogicTest1()
+        {
+            CalculateDisplcaement objDisplcaement = new CalculateDisplcaement();
+            int[,] inputarray = new int[4, 4] { { 0, 1, 1, 0 }, { 1, 1, 1, 1 }, { 1, 1, 0, 1 }, { 0, 1, 1, 1 } };
+            Point p=new Point(0,1);
+            Point q = new Point(3, 2);
+            string expetedOutput = "Yes 4";
+            string actualOutput=objDisplcaement.CalculateLogic(inputarray, p, q);
+            Assert.AreEqual(actualOutput, expetedOutput);
+
+    }
+        [TestMethod()]
+        public void CalculateLogicTest2()
+        {
+            CalculateDisplcaement objDisplcaement = new CalculateDisplcaement();
+            int[,] inputarray = new int[4, 4] { { 0, 1, 1, 0 }, { 1, 0, 1, 1 }, { 1, 1, 0, 1 }, { 0, 0, 1, 1 } };
+            Point p = new Point(0, 1);
+            Point q = new Point(2, 1);
+            string expetedOutput = "No";
+            string actualOutput = objDisplcaement.CalculateLogic(inputarray, p, q);
+            Assert.AreEqual(actualOutput, expetedOutput);
+
+        }
+        [TestMethod()]
+        public void CalculateLogicTest3()
+        {
+            CalculateDisplcaement objDisplcaement = new CalculateDisplcaement();
+            int[,] inputarray = new int[4, 4] { { 1, 1, 1, 1 }, { 1, 1, 1, 1 }, { 1, 1, 1, 1 }, { 1, 1, 1, 1 } };
+            Point p = new Point(0, 0);
+            Point q = new Point(3, 3);
+            string expetedOutput = "Yes 6";
+            string actualOutput = objDisplcaement.CalculateLogic(inputarray, p, q);
+            Assert.AreEqual(actualOutput, expetedOutput);
+
+        }
+    }
+}
