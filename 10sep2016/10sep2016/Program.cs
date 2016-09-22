91b5e6256ab6bcf00855125cdcc0fcd76175fcf6
gades <sarathchandra.gadepalli@campsystems.com>
2016-09-23 00:42:17 +0530

    Changes for taking the proper input

--- 10sep2016/10sep2016/Program.cs
+++ 10sep2016/10sep2016/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +17,7 @@ namespace _10sep2016
         {
             TakeInputandcalculateLogic();
             Console.ReadLine();
+            
         }
 
         public string CalculateLogic(int[,] pointsarray, Point p, Point q)
@@ -94,7 +96,6 @@ namespace _10sep2016
                         }
                     }
                 }
-
             }
             catch (IndexOutOfRangeException ex)
             {
@@ -108,45 +109,61 @@ namespace _10sep2016
 
         private static void TakeInputandcalculateLogic()
         {
-            try { 
-            Console.WriteLine("please enter the number of rows");
-            var noofRows = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                Console.WriteLine("please enter the number of rows");
+                var noofRows = Convert.ToInt32(Console.ReadLine());
                 if (noofRows < 0 || noofRows >= 100)
                 {
                     Console.WriteLine("no of rows must be greater than 0 and less than 100");
                     noofRows = Convert.ToInt32(Console.ReadLine());
                 }
-            Console.WriteLine("please enter the number of columns");
-            var noofColumns = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("please enter the number of columns");
+                var noofColumns = Convert.ToInt32(Console.ReadLine());
                 if (noofColumns < 0 || noofColumns >= 100)
                 {
                     Console.WriteLine("no of columns must be greater than 0 and less than 100");
                     noofColumns = Convert.ToInt32(Console.ReadLine());
                 }
-                Console.WriteLine("please enter the elements { each element followed by enter } ");
-            int[,] pointsarray = new int[noofRows, noofColumns];
-            int i;
+                Console.WriteLine("please enter the elements {Format : x1 x2 x3 .... } ");
+                int[,] pointsarray = new int[noofRows, noofColumns];
+                int i, k = 0;
                 /* output each array element's value */
+                var inpurArray = Console.ReadLine().Trim();
+                string[] inputvalues = inpurArray != null ? inpurArray.Split() : null;
+                int[] inputarrayvalues = Array.ConvertAll(inputvalues, int.Parse);
+                if (inputarrayvalues.Length != noofRows * noofColumns)
+                {
+                    Console.WriteLine("number of inputs must be equal to rows * columns");
+                    return;
+                }
                 for (i = 0; i < noofRows; i++)
                 {
                     int j;
                     for (j = 0; j < noofColumns; j++)
                     {
-                        pointsarray[i, j] = Convert.ToInt32(Console.ReadLine());
+                        pointsarray[i, j] = inputarrayvalues[j + k];
                         if (pointsarray[i, j] != 0 && pointsarray[i, j] != 1)
                         {
                             Console.WriteLine("please enter only 0's and 1's as input");
                             return;
                         }
                     }
+                    k += j;
                 }
-                Console.WriteLine("please enter  two input points in array { each element followed by enter } ");
+                Console.WriteLine("please enter  two input points in array {fomat: p1 q1 next line  p2 q2} ");
                 Point p = new Point();
                 Point q = new Point();
-                p.X = Convert.ToInt32(Console.ReadLine());
-                p.Y = Convert.ToInt32(Console.ReadLine());
-                q.X = Convert.ToInt32(Console.ReadLine());
-                q.Y = Convert.ToInt32(Console.ReadLine());
+                var point1 = Console.ReadLine();
+                string[] tokens = point1 != null ? point1.Split() : null;
+                int[] numbers = Array.ConvertAll(tokens, int.Parse);
+                p.X = numbers[0];
+                p.Y = numbers[1];
+                var point2 = Console.ReadLine();
+                string[] tokens2 = point2 != null ? point2.Split() : null;
+                int[] numbers2 = Array.ConvertAll(tokens2, int.Parse);
+                q.X = numbers2[0];
+                q.Y = numbers2[1];
                 CalculateDisplcaement displacement = new CalculateDisplcaement();
                 displacement.CalculateLogic(pointsarray, p, q);
             }
